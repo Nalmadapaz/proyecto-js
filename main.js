8ac3220e4777a8eeb25a664c0b0490b36e0ff94d
nahuelalmadapaz <nahuel.almada@hotmail.com>
2023-10-01 13:08:29 -0300

    Metodos

--- main.js
+++ main.js
@@ -17,23 +17,32 @@ function simulador() {
   console.log("¡Bienvenido a mi tienda!");
   mostrarProductos();
 
-  const seleccion = parseInt(prompt("Elija un número de producto (1-5):"));
+  let seleccion;
+  let productoElegido;
 
-  if (seleccion >= 1 && seleccion <= 5) {
-    const productoElegido = productos[seleccion - 1];
-    console.log(`Ha seleccionado: ${productoElegido.nombre}`);
+  while (true) {
+    seleccion = parseInt(prompt("Elija un número de producto (1-5):"));
 
-    const cantidad = parseInt(
-      prompt(`¿Cuántas unidades de ${productoElegido.nombre} desea comprar?`)
-    );
-
-    const costoTotal = productoElegido.precio * cantidad;
-    console.log(`El costo total es: $${costoTotal}`);
-  } else {
-    console.log(
-      "Selección no válida. Por favor, elija un número de producto entre 1 y 5."
-    );
+    if (seleccion >= 1 && seleccion <= 5) {
+      productoElegido = productos.find((_, index) => index === seleccion - 1);
+      console.log(`Ha seleccionado: ${productoElegido.nombre}`);
+      break;
+    } else {
+      console.log(
+        "Selección no válida. Por favor, elija un número de producto entre 1 y 5."
+      );
+    }
   }
+
+  const cantidad = parseInt(
+    prompt(`¿Cuántas unidades de ${productoElegido.nombre} desea comprar?`)
+  );
+
+  const costoTotal = productoElegido.precio * cantidad;
+  console.log(`El costo total es: $${costoTotal}`);
+
+  const carrito = [productoElegido];
+  console.log("Carrito de compra:", carrito);
 }
 
 simulador();
