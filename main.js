6c25b460ac687aac78685f4ec84f835421a4ac27
nahuelalmadapaz <nahuel.almada@hotmail.com>
2023-10-14 14:28:27 -0300

    DOM y JSON

--- main.js
+++ main.js
@@ -1,48 +1,45 @@
 const productos = [
-  { nombre: "Producto 1", precio: 100 },
-  { nombre: "Producto 2", precio: 150 },
-  { nombre: "Producto 3", precio: 170 },
-  { nombre: "Producto 4", precio: 222 },
-  { nombre: "Producto 5", precio: 500 },
+  { nombre: "Nyke Phantom", precio: 100 },
+  { nombre: "Nyker Mercurial", precio: 150 },
+  { nombre: "Nyke Premier", precio: 170 },
+  { nombre: "Nyke Tiempo", precio: 222 },
+  { nombre: "Nyke Phantom GX", precio: 500 },
 ];
 
 function mostrarProductos() {
-  console.log("Lista de productos disponibles:");
-  for (let i = 0; i < productos.length; i++) {
-    console.log(`${i + 1}. ${productos[i].nombre} - $${productos[i].precio}`);
+  const productosContainer = document.getElementById("productos-container");
+  productosContainer.innerHTML = '<h2>Lista de productos disponibles:</h2>';
+
+  productos.forEach((producto,index) => {
+    const productoDiv = document.createElement("div");
+    productoDiv.innerHTML = `${index + 1}. ${producto.nombre} - $${producto.precio}`;
+    productosContainer.appendChild(productoDiv);
+  })
+}
+
+function manejarSeleccion() {
+  const seleccionInput = document.getElementById("seleccion-input");
+  const seleccion = parseInt(seleccionInput.value);
+
+  if (seleccion >= 1 && seleccion <= 5) {
+    const productoElegido = productos.find((_, index) => index === seleccion - 1);
+    mostrarMensaje(`Ha seleccionado: ${productoElegido.nombre}`);
+  } else {
+    mostrarMensaje("Selección no válida. Por favor, elija un número de producto entre 1 y 5.");
   }
 }
 
-function simulador() {
-  console.log("¡Bienvenido a mi tienda!");
+function mostrarMensaje(mensaje) {
+  const mensajeDiv = document.createElement("div");
+  mensajeDiv.innerHTML = `<p>${mensaje}</p>`;
+  document.body.appendChild(mensajeDiv);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
   mostrarProductos();
-
-  let seleccion;
-  let productoElegido;
-
-  while (true) {
-    seleccion = parseInt(prompt("Elija un número de producto (1-5):"));
-
-    if (seleccion >= 1 && seleccion <= 5) {
-      productoElegido = productos.find((_, index) => index === seleccion - 1);
-      console.log(`Ha seleccionado: ${productoElegido.nombre}`);
-      break;
-    } else {
-      console.log(
-        "Selección no válida. Por favor, elija un número de producto entre 1 y 5."
-      );
-    }
-  }
-
-  const cantidad = parseInt(
-    prompt(`¿Cuántas unidades de ${productoElegido.nombre} desea comprar?`)
-  );
-
-  const costoTotal = productoElegido.precio * cantidad;
-  console.log(`El costo total es: $${costoTotal}`);
-
-  const carrito = [productoElegido];
-  console.log("Carrito de compra:", carrito);
-}
-
-simulador();
+  const seleccionForm = document.getElementById("seleccion-form");
+  seleccionForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    manejarSeleccion();
+  });
+});
